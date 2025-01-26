6d55951d606bda06d6fcc22100e183540bb3c10a
Oleksiy <Unialex@ukr.net>
2025-01-26 11:55:40 +0200

    done

--- js/lesson2.js
+++ js/lesson2.js
@@ -35,7 +35,6 @@
 
 // checkLogin(logins);
 
-
 // 3. Напишіть функцію, яка складатиме сусідні числа і пушитиме їх в новий масив
 // const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
 
@@ -57,15 +56,41 @@
 // аргументів і повертає їхнє середнє значення.
 // Додати перевірку, що аргументи це числа.
 
-function calculateAverage() {
-  let sum = 0;
-  let i = 0;
-  for (const arg of arguments) {
-    if (typeof arg === "number") {
-      sum += arg;
-      i += 1;
-    }
-  }
-  return sum / i || 0;
+// function calculateAverage() {
+//   let sum = 0;
+//   let i = 0;
+//   for (const arg of arguments) {
+//     if (typeof arg === "number") {
+//       sum += arg;
+//       i += 1;
+//     }
+//   }
+//   return sum / i || 0;
+// }
+// console.log(calculateAverage("hello"));
+
+// 7. Напиши скрипт, який для об'єкту user,
+// послідовно:
+// 1 - додасть поле mood зі значенням 'happy'
+// 2 - замінить hobby на 'skydiving'
+// 3 - замінить значення premium на false
+// 4 - виводить зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tenis",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(`${key}:${user[key]}`);
+// }
+const entrys = Object.entries(user);
+for (const entry of entrys) {
+  //console.log(entry);
+  console.log(`${entry[0]}:${entry[1]}`);
 }
-console.log(calculateAverage("hello"));
