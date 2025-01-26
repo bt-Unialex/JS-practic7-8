412414852b79800c281810a2dd02d9d982164b64
Inna-Vinnyk <vinnik_inna@ukr.net>
2025-01-26 11:05:33 +0100

    add task-6

--- js/lesson2.js
+++ js/lesson2.js
@@ -76,21 +76,38 @@
 // 3 - замінить значення premium на false
 // 4 - виводить зміст об'єкта user у форматі
 // '<ключ>:<значення>' використовуя Object.keys() та for...of
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tenis",
-  premium: true,
-};
-user.mood = "happy";
-user.hobby = "skydiving";
-user.premium = false;
-// const keys = Object.keys(user);
-// for (const key of keys) {
-//   console.log(`${key}:${user[key]}`);
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tenis",
+//   premium: true,
+// };
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+// // const keys = Object.keys(user);
+// // for (const key of keys) {
+// //   console.log(`${key}:${user[key]}`);
+// // }
+// const entrys = Object.entries(user);
+// for (const entry of entrys) {
+//   //console.log(entry);
+//   console.log(`${entry[0]}:${entry[1]}`);
 // }
-const entrys = Object.entries(user);
-for (const entry of entrys) {
-  //console.log(entry);
-  console.log(`${entry[0]}:${entry[1]}`);
+
+// 8. Є об'єкт, в якому зберігаються зарплати команди
+// Напишіть код для додавання усіх зарплат та
+// збережіть його результат в змінній sum.
+// Якщо об'єкт salaries пустий, то результат має бути 0
+
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
+};
+let sum = 0;
+const totalSalaries = Object.values(salaries);
+for (const totalSalarie of totalSalaries) {
+  sum += totalSalarie;
 }
+console.log(sum);
